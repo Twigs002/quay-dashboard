fcffd117c3810021d19e8018c67e0de5623984c6
Twigs002 <paganvniekerk22@gmail.com>
2026-05-05 12:03:40 +0200

    Fix return indentation in build_timespan (restore 8-space indent)

--- scripts/fetch_dialfire.py
+++ scripts/fetch_dialfire.py
@@ -59,7 +59,7 @@ def build_timespan(period_start, period_end, now_sast):
         days_to_end = 0
     else:               # Tue-Fri: end at yesterday to get reliable 200 response
         days_to_end = 1
-return f"{days_to_start}-{days_to_end}day"
+        return f"{days_to_start}-{days_to_end}day"
 
 
 # -- Poll helper --------------------------------------------------------------
